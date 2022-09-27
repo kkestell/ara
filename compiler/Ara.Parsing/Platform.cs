c2ade5177a3f2b89edfad6c4504e374d111c393d
Kyle Kestell <kyle@kestell.org>
2022-09-27 13:55:21 -0500

    Darwin -> OSX

--- compiler/Ara.Parsing/Platform.cs
+++ compiler/Ara.Parsing/Platform.cs
@@ -4,8 +4,8 @@ public static class Platform
 {
 #if _WINDOWS
     public const string SharedLibrary = "Ara.Parsing.Windows-x86_64.dll";
-#elif _DARWIN
-    public const string SharedLibrary = "Ara.Parsing.Darwin-x86_64.dylib";
+#elif _OSX
+    public const string SharedLibrary = "Ara.Parsing.OSX-x86_64.dylib";
 #elif _LINUX
     public const string SharedLibrary = "Ara.Parsing.Linux-x86_64.so";
 #endif
